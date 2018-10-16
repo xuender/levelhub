68f53afa32340c6b2fa0f9c693487ee720660a21
ender <xuender@gmail.com>
2018-10-16 16:24:35 +0800

    docs:README.md and example

--- example/close.go
+++ example/close.go
@@ -1,12 +1,3 @@
-# README
-levelhub is a wrapper of leveldb which supports multi-tenancy
-
-## Installation
-```shell
-go get -u github.com/xuender/levelhub
-```
-## Usage:
-```go
 package main
 
 import "github.com/xuender/levelhub"
@@ -17,4 +8,3 @@ func main() {
 	hub.Put(1, []byte("key"), []byte("val"), nil)
 	hub.Put(2, []byte("key"), []byte("val"), nil)
 }
-```
